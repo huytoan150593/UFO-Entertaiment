a2bb6e27ce49166a1c5d3b368b513636e53a670e
huytoan150593 <mrtoan150593@gmail.com>
2022-09-07 20:24:35 +0700

    update data

--- src/components/CreateTeam/CreateTeam.jsx
+++ src/components/CreateTeam/CreateTeam.jsx
@@ -2,7 +2,26 @@ import React from "react";
 import "./CreateTeam.css";
 
 const CreateTeam = () => {
-  return <div id="create-team">CreateTeam</div>;
+  return (
+    <div id="create-team">
+      <div className="talent">
+        <div className="talent-image">
+          <img src="/assets/talent/hao-sq.jpg" alt="" />
+        </div>
+        <div className="talent-info">
+          <h1>Hoang Gia Hao</h1>
+          <p>Birthday: 17/05/1996</p>
+          <p>Position: Creater</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            nihil, reiciendis minima, beatae aspernatur dolores vero ipsum
+            tenetur dolor natus sit eum voluptate aliquid. Cupiditate illum eum
+            rerum tempore cumque.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateTeam;
--- src/constains/data.js
+++ src/constains/data.js
@@ -47,106 +47,66 @@ export const musicVideo = [
         singer: "Mr B",
         thumbnail: "/assets/Thumbnail/6.jpg",    
     },
-]
-
-export const musicVideo1 = [
     {
-        id: 1,
-        name: "BẢN TÌNH CA MÙA ĐÔNG",
-        url: "https://www.youtube.com/embed/Yp4kzdbNuIE",
-        type: "Cover",
-        singer: "Mr B",
-        thumbnail: "/assets/Thumbnail/1.jpg",
+        id: 7,
+        name: "Đắng nồng cay",
+        url: "https://www.youtube.com/embed/9EziZMELz_k",
+        type: "Ballad Version",
+        singer: "Yen Tattoo",
+        thumbnail: "/assets/Thumbnail/7.jpg",    
     },
     {
-        id: 2,
-        name: "ĐÁ LÔNG NHEO",
-        url: "https://www.youtube.com/embed/qFFUSveH7VE",
-        type: "Offical Music Video",
-        singer: "Gusty",
-        thumbnail: "/assets/Thumbnail/2.jpg",
-    },
-    {
-        id: 3,
-        name: "HÃY VỀ ĐÂY BÊN ANH",
-        url: "https://www.youtube.com/embed/fTgsJM_4GjA",
-        type: "Music Video",
-        singer: "Lien Binh Phat",
-        thumbnail: "/assets/Thumbnail/3.jpg",
-    },
-    {
-        id: 4,
-        name: "NẤC THANG LÊN THIÊN ĐƯỜNG",
-        url: "https://www.youtube.com/embed/foc5tYsyaXU",
-        type: "Cover",
-        singer: "Mr B",
-        thumbnail: "/assets/Thumbnail/4.jpg",
-    },
-    {
-        id: 5,
-        name: "NHƯ MÙA TUYẾT ĐẦU TIÊN",
-        url: "https://www.youtube.com/embed/qfdxApg0Z_o",
-        type: "Cover",
-        singer: "Mr B",
-        thumbnail: "/assets/Thumbnail/5.jpeg",
-    },
-    {
-        id: 6,
-        name: "XIN LỖI ANH YÊU EM",
+        id: 8,
+        name: "Only",
         url: "https://www.youtube.com/embed/9EziZMELz_k",
         type: "Cover",
         singer: "Mr B",
-        thumbnail: "/assets/Thumbnail/6.jpg",    
+        thumbnail: "/assets/Thumbnail/8.jpg",    
     },
 ]
 
-export const musicVideo2 = [
+export const tvc = [
     {
         id: 1,
-        name: "BẢN TÌNH CA MÙA ĐÔNG",
-        url: "https://www.youtube.com/embed/Yp4kzdbNuIE",
-        type: "Cover",
-        singer: "Mr B",
-        thumbnail: "/assets/Thumbnail/1.jpg",
+        name: "TVC Commerce",
+        url: "https://www.youtube.com/embed/9EziZMELz_k",
+        type: "Promote",
+        singer: "Trang",
+        thumbnail: "/assets/Thumbnail/tvc.png"
+    }
+]
+
+export const viralVideo = [
+    {
+        id: 1,
+        name: "Nest Cafe",
+        url: "https://www.youtube.com/embed/tgbNymZ7vqY",
+        type: "promotion",
+        singer: "Lien Binh Phat",
+        thumbnail: "/assets/Thumbnail/vr1.png"
     },
     {
         id: 2,
-        name: "ĐÁ LÔNG NHEO",
-        url: "https://www.youtube.com/embed/qFFUSveH7VE",
-        type: "Offical Music Video",
-        singer: "Gusty",
-        thumbnail: "/assets/Thumbnail/2.jpg",
+        name: "Nest Cafe",
+        url: "https://www.youtube.com/embed/tgbNymZ7vqY",
+        type: "promotion",
+        singer: "Lien Binh Phat",
+        thumbnail: "/assets/Thumbnail/vr2.png"
     },
     {
         id: 3,
-        name: "HÃY VỀ ĐÂY BÊN ANH",
-        url: "https://www.youtube.com/embed/fTgsJM_4GjA",
-        type: "Music Video",
+        name: "Nest Cafe",
+        url: "https://www.youtube.com/embed/tgbNymZ7vqY",
+        type: "promotion",
         singer: "Lien Binh Phat",
-        thumbnail: "/assets/Thumbnail/3.jpg",
+        thumbnail: "/assets/Thumbnail/vr3.jpg"
     },
     {
         id: 4,
-        name: "NẤC THANG LÊN THIÊN ĐƯỜNG",
-        url: "https://www.youtube.com/embed/foc5tYsyaXU",
-        type: "Cover",
-        singer: "Mr B",
-        thumbnail: "/assets/Thumbnail/4.jpg",
-    },
-    {
-        id: 5,
-        name: "NHƯ MÙA TUYẾT ĐẦU TIÊN",
-        url: "https://www.youtube.com/embed/qfdxApg0Z_o",
-        type: "Cover",
-        singer: "Mr B",
-        thumbnail: "/assets/Thumbnail/5.jpeg",
-    },
-    {
-        id: 6,
-        name: "XIN LỖI ANH YÊU EM",
-        url: "https://www.youtube.com/embed/9EziZMELz_k",
-        type: "Cover",
-        singer: "Mr B",
-        thumbnail: "/assets/Thumbnail/6.jpg",    
+        name: "Nest Cafe",
+        url: "https://www.youtube.com/embed/tgbNymZ7vqY",
+        type: "promotion",
+        singer: "Lien Binh Phat",
+        thumbnail: "/assets/Thumbnail/vr4.png"
     },
 ]
